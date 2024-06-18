f11983527efaf2f4d4f3b2bf734d63e8153c2b40
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-06-18 09:59:58 +0200

    commit

---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { IoIosMore } from 'react-icons/io';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+
+const Ecommerce = () => {
+  return (
+    <div>Ecommerce</div>
+  )
+}
+
+export default Ecommerce
+// 1:41:00
