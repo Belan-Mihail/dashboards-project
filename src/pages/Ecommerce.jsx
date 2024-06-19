0c0f0f90e5dd6d85eacbe900d3851b459a8752fe
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-06-19 08:42:56 +0200

    add additional html to ecommerc component

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -34,11 +34,35 @@ const Ecommerce = () => {
 
           </div>
         </div>
+
+        <div className='flex gap-1 m-3 flex-wrap justify-center items-center'>
+            {earningData.map((item) => (
+              <div key={item.title} className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-4 pt-9 rounded-2xl md:w-56  '>
+                  <button type='button' style={{color: item.iconColor, background: item.iconBg}} className='text-2xl rounded-full p-4 opacity-0.9 hover:drop-shadow-xl'>
+                    {item.icon}
+
+                  </button>
+                  <p className='mt-3'>
+                    <span className='text-xl font-semibold'>
+                        {item.amount}
+                    </span>
+                    <span className={`text-sm text-${item.pcColor} ml-2`}>
+                      {item.percentage}
+                    </span>
+                  </p>
+                  <p className='mt-1 text-sm text-gray-400'>
+                    {item.title}
+                  </p>
+              </div>
+            ))}
+        </div>
       </div>
-      
+            <div>
+              
+            </div>
     </div>
   )
 }
 
 export default Ecommerce
-// 1:41:00
+// 1:48:30
