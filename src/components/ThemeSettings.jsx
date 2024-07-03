0ddccf92b8b122e52cac7b7127e63ef303e1d174
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-03 09:46:27 +0200

    add basic html and styles to ThemeSettings component

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -8,7 +8,27 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 const ThemeSettings = () => {
   return (
-    <div>ThemeSettings</div>
+    <div className='bg-half-transparent w-screen fixed nav-item top-0 right-0'>
+      <div className='float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400'>
+        <div className='flex justify-center items-center p-4 ml-4'>
+          <p className='font-semibold text-xl'>Settings</p>
+          <button type='button' onClick={()=> {}} style={{color: 'rgb(153, 171, 180)', borderRadius: '50%'}} className=' hover:bg-light-gray text-2xl p-3 hover:drop-shadow-sm'>
+              <MdOutlineCancel />
+          </button>
+        </div>
+        <div className='flex flex-col border-t-1 border-color p-4 ml-4'>
+            <p className='font-semibold text-lg'>Theme Optioms</p>
+            <div className='mt-4'>
+              <input type="radio" id='light' name='theme' value='Light' className='cursor-pointer' onChange={()=> {}} checked={true}  />
+              <label htmlFor="light" className='ml-2 text-md cursor-pointer'>Light</label>
+            </div>
+            <div className='mt-4'>
+              <input type="radio" id='dark' name='theme' value='Dark' className='cursor-pointer' onChange={()=> {}} checked={true}  />
+              <label htmlFor="dark" className='ml-2 text-md cursor-pointer'>Dark</label>
+            </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
