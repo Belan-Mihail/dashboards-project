362eebaf3968a52f294173ce723233607ba584db
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-03 09:33:20 +0200

    start create themesettings components

--- src/App.js
+++ src/App.js
@@ -65,6 +65,7 @@ function App() {
             </div>
 
             <div>
+              <ThemeSettings />
               <Routes>
                 {/* dashboard */}
                 <Route path="/" element={<Ecommerce />} />
