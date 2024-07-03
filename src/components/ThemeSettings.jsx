4dc45d51126ad0213c257ec47a629a23e42b71ee
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-07-03 10:01:06 +0200

    finish add styling to the ThemeSrtting component

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -27,6 +27,23 @@ const ThemeSettings = () => {
               <label htmlFor="dark" className='ml-2 text-md cursor-pointer'>Dark</label>
             </div>
         </div>
+        <div className='flex flex-col border-t-1 border-color p-4 ml-4'>
+            <p className='font-semibold text-lg'>Theme Colors</p>
+            <div className=' flex gap-3'>
+              {themeColors.map((item, index) => (
+                <TooltipComponent key={index} content={item.name} position='TopCenter'>
+                  <div className='relative mt-2 flex gap-5 items-center cursor-pointer'>
+                    <button type='button' className='h-10 w-10 rounded-full cursor-pointer' style={{backgroundColor: item.color}} onClick={()=> {}}>
+                      <BsCheck className={`ml-2 text-2xl text-white ${true ? 'block' : 'hidden'}`} />
+                    </button>
+
+                  </div>
+
+                </TooltipComponent>
+              ))}
+
+            </div>
+        </div>
       </div>
     </div>
   )
